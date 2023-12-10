f5d1ba064c526ea117b1de33982738ffc05e6292
colin <csn23@ic.ac.uk>
2023-12-10 19:48:37 +0000

    fixed bug on json parse

--- api/app.py
+++ api/app.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 
 import requests
 from dotenv import load_dotenv
-from flask import (Flask, abort, redirect, render_template, request,
-                   session, url_for)
+from flask import (Flask, abort, redirect, render_template, request, session,
+                   url_for)
 from flask_login import (LoginManager, current_user, login_required,
                          login_user, logout_user)
 from oauthlib.oauth2 import WebApplicationClient
